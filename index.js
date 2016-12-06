739d8bd3bd04818192eaa8f662c21a148271c828
JasonMarkWomack <jwomack155@gmail.com>
2016-12-06 17:47:17 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+var express  =  require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function (req, res, next) {
+  console.log(req.body);
+});
+
+app.post('/messages', function (req, res, next) {
+  messages.push(req.body.message);
+  res.status(200).json({ messages: messages });
+
+});
+
+
+app.listen(3000, function(){
+console.log("working & listening on port 3000!");
+});
